15df5a3a8167472d1dcbfacca23c7948261c8c01
dmstepanuik <0669218646@ukr.net>
2023-02-16 15:48:47 +0200

    feat: add tab navigate

--- src/Index.jsx
+++ src/Index.jsx
@@ -5,6 +5,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import RegistrationScreen from './screens/RegistrationScreen/RegistrationScreen';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 import mainTab from './variables/mainTab';
+import { Text } from 'react-native';
+import ProfileScreen from './screens/ProfileScreen/ProfileScreen';
+import CreatePostsScreen from './screens/CreatePostsScreen/CreatePostsScreen';
+import PostsScreen from './screens/PostsScreen/PostsScreen';
 
 export default function App() {
   const [isAuth, setIsAuth] = useState(false);
@@ -26,6 +30,37 @@ export default function App() {
         </AuthStack.Navigator>
       )}
 
+      {isAuth && (
+        <MainTab.Navigator
+          initialRouteName="posts"
+          screenOptions={mainTab.screenOptions}
+        >
+
+          <MainTab.Screen
+            name="posts"
+            component={PostsScreen}
+            options={mainTab.options.getPosts(setIsAuth)}
+          />
+
+          <MainTab.Screen
+            name="create"
+            options={({ navigation }) =>
+              mainTab.options.getPostCreation(navigation)
+            }
+          >
+            {() => (
+              <CreatePostsScreen
+                imgUrl={require('./assets/images/posts/img01.jpg')}
+              />
+            )}
+          </MainTab.Screen>
+          <MainTab.Screen
+            name="profile"
+            component={ProfileScreen}
+            options={mainTab.options.profile}
+          />
+        </MainTab.Navigator>
+      )}
     </NavigationContainer>
   );
 }
